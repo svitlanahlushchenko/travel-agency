1450aecf32501dcfdd325ed8faca3a507a268a7c
Svitlana <shumovasveta27@gmail.com>
2024-11-10 15:36:09 +0100

    hotels-section added

---
+++ src/components/Slider.js
@@ -0,0 +1,277 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+const NextArrow = props => {
+  const { onClick } = props;
+  return (
+    <button className='next-button' onClick={onClick}>
+      <svg className='next-arrow'>
+        <use xlinkHref='/images/icons.svg#icon-triangle-arrow'></use>
+      </svg>
+    </button>
+  );
+};
+
+const PrevArrow = props => {
+  const { onClick } = props;
+  return (
+    <button className='prev-button' onClick={onClick}>
+      <svg className='prev-arrow'>
+        <use xlinkHref='/images/icons.svg#icon-triangle-arrow'></use>
+      </svg>
+    </button>
+  );
+};
+
+const HotelsSlider = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <Slider {...settings}>
+      <div className='slide'>
+        <div className='photo-container'>
+          <img
+            src='/images/Eclipse-Haven.jpg'
+            alt='Photo of Hotel'
+            className='hotel-photo'
+          ></img>
+          <img
+            src='/images/Breakfast-Included.jpg'
+            alt='Breakfast Included'
+            className='breakfast-photo'
+          ></img>
+        </div>
+        <div className='slide-info'>
+          <h3 className='slide-title'>Eclipse Haven Lodge</h3>
+          <div className='slide-address'>
+            <svg className='address-logo'>
+              <use xlinkHref='/images/icons.svg#icon-address'></use>{' '}
+            </svg>
+            <p>
+              House 168/170, Road 02, Avenue 01, Mirpur DOHS, Dhaka, Bangladesh
+            </p>
+            <a
+              className='link-map'
+              href='https://www.google.com/maps?q=House+168/170,+Road+02,+Avenue+01,+Mirpur+DOHS,+Dhaka,+Bangladesh'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              See Map
+            </a>
+          </div>
+          <div className='services'>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-tv'></use>{' '}
+              </svg>
+              <p>TV</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-heater'></use>{' '}
+              </svg>
+              <p>Heater</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-safe'></use>{' '}
+              </svg>
+              <p>Saving Safe</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-wi-fi'></use>{' '}
+              </svg>
+              <p>Free Wifi</p>
+            </div>
+          </div>
+          <div className='service'>
+            <svg className='service-logo'>
+              <use xlinkHref='/images/icons.svg#icon-home-phone'></use>{' '}
+            </svg>
+            <p>Phone</p>
+          </div>
+          <div className='hotel-room'>
+            <div className='room'>
+              <h4 className='room-title'>Sea View</h4>
+              <h4 className='room-title'>Semi Double</h4>
+              <p>1 king bed</p>
+              <p className='accent-text'>Free cancellation</p>
+              <p>before 48 hours</p>
+            </div>
+            <div className='price'>
+              <p>1 Night, 2 Adults</p>
+              <p className='accent-price'>
+                $126<span className=' crossed-price'>$160</span>
+              </p>
+              <button className='book-button'>Check Availability</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='slide'>
+        <img
+          src='/images/Twilight-Serenity.jpg'
+          alt='Photo of Hotel'
+          className='hotel-photo'
+        ></img>
+        <img
+          src='/images/Breakfast-Included.jpg'
+          alt='Breakfast Included'
+          className='breakfast-photo'
+        ></img>
+        <div className='slide-info'>
+          <h3 className='slide-title'>Twilight Serenity Manore</h3>
+          <div className='slide-address'>
+            <svg className='address-logo'>
+              <use xlinkHref='/images/icons.svg#icon-address'></use>{' '}
+            </svg>
+            <p>
+              House 168/170, Road 02, Avenue 01, Mirpur DOHS, Dhaka, Bangladesh
+            </p>
+            <a
+              className='link-map'
+              href='https://www.google.com/maps?q=House+168/170,+Road+02,+Avenue+01,+Mirpur+DOHS,+Dhaka,+Bangladesh'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              See Map
+            </a>
+          </div>
+          <div className='services'>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-tv'></use>{' '}
+              </svg>
+              <p>TV</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-heater'></use>{' '}
+              </svg>
+              <p>Heater</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-safe'></use>{' '}
+              </svg>
+              <p>Saving Safe</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-wi-fi'></use>{' '}
+              </svg>
+              <p>Free Wifi</p>
+            </div>
+          </div>
+          <div className='service'>
+            <svg className='service-logo'>
+              <use xlinkHref='/images/icons.svg#icon-home-phone'></use>{' '}
+            </svg>
+            <p>Phone</p>
+          </div>
+          <div className='hotel-room'>
+            <div className='room'>
+              <h4 className='room-title'>Family Room</h4>
+              <h4 className='room-title'>Quine Modern Room</h4>
+              <h4 className='room-title'>Semi Double</h4>
+              <p>1 king bed</p>
+              <p className='accent-text'>Free cancellation</p>
+              <p>before 48 hours</p>
+            </div>
+            <div className='price'>
+              <p>1 Night, 2 Adults</p>
+              <p className='accent-price'>$46</p>
+              <button className='book-button'>Check Availability</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='slide'>
+        <img
+          src='/images/Dusk-Retreat.jpg'
+          alt='Photo of Hotel'
+          className='hotel-photo'
+        ></img>
+        <div className='slide-info'>
+          <h3 className='slide-title'>Dusk Retreat Resort</h3>
+          <div className='slide-address'>
+            <svg className='address-logo'>
+              <use xlinkHref='/images/icons.svg#icon-address'></use>{' '}
+            </svg>
+            <p>
+              House 168/170, Road 02, Avenue 01, Mirpur DOHS, Dhaka, Bangladesh
+            </p>
+            <a
+              className='link-map'
+              href='https://www.google.com/maps?q=House+168/170,+Road+02,+Avenue+01,+Mirpur+DOHS,+Dhaka,+Bangladesh'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              See Map
+            </a>
+          </div>
+          <div className='services'>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-tv'></use>{' '}
+              </svg>
+              <p>TV</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-heater'></use>{' '}
+              </svg>
+              <p>Heater</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-safe'></use>{' '}
+              </svg>
+              <p>Saving Safe</p>
+            </div>
+            <div className='service'>
+              <svg className='service-logo'>
+                <use xlinkHref='/images/icons.svg#icon-wi-fi'></use>{' '}
+              </svg>
+              <p>Free Wifi</p>
+            </div>
+          </div>
+          <div className='service'>
+            <svg className='service-logo'>
+              <use xlinkHref='/images/icons.svg#icon-home-phone'></use>{' '}
+            </svg>
+            <p>Phone</p>
+          </div>
+          <div className='hotel-room'>
+            <div className='room'>
+              <h4 className='room-title'>Couple Room</h4>
+              <h4 className='room-title'>Family Room</h4>
+              <p>1 king bed</p>
+              <p className='accent-text'>Free cancellation</p>
+              <p>before 48 hours</p>
+            </div>
+            <div className='price'>
+              <p>1 Night, 2 Adults</p>
+              <p className='accent-price'>$120 </p>
+              <button className='book-button'>Check Availability</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default HotelsSlider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,129 +1,161 @@
 import React from 'react';
 import '../css/App.css';
+import HotelsSlider from '../components/Slider';
 
 const Home = () => {
   return (
     <section>
-      <>
-        <section className='hero-section big-container'>
-          <div className='hero-section__container'>
-            <div className='hero-section__text'>
-              <img src='/images/Mexico-city.svg' alt='Mexico City'></img>
-              <h1 className='hero-section__title'>
-                Discover Your{' '}
-                <span className='hero-section__accent'>Future Vacation</span>{' '}
-                Specials!
-              </h1>
+      <section className='hero-section big-container'>
+        <div className='hero-section__container'>
+          <div className='hero-section__text'>
+            <img src='/images/Mexico-city.svg' alt='Mexico City'></img>
+            <h1 className='hero-section__title'>
+              Discover Your{' '}
+              <span className='hero-section__accent'>Future Vacation</span>{' '}
+              Specials!
+            </h1>
+          </div>
+          <div className='hero-section__offers'>
+            <div className='hero-section__offer'>
+              <svg className='hero-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-tour'></use>{' '}
+              </svg>
+              <p className='hero-section__offer-name'>Tour</p>
             </div>
-            <div className='hero-section__offers'>
-              <div className='hero-section__offer'>
-                <svg className='hero-section__icon'>
-                  <use xlinkHref='/images/icons.svg#icon-tour'></use>{' '}
-                </svg>
-                <p className='hero-section__offer-name'>Tour</p>
-              </div>
-              <div className='hero-section__offer'>
-                <svg className='hero-section__icon'>
-                  <use xlinkHref='/images/icons.svg#icon-hotel'></use>{' '}
-                </svg>
-                <p className='hero-section__offer-name'>Hotel</p>{' '}
-              </div>
-              <div className='hero-section__offer'>
-                <svg className='hero-section__icon'>
-                  <use xlinkHref='/images/icons.svg#icon-visa'></use>{' '}
-                </svg>
-                <p className='hero-section__offer-name'>Visa</p>
-              </div>
-              <div className='hero-section__offer'>
-                <svg className='hero-section__icon'>
-                  <use xlinkHref='/images/icons.svg#icon-activities'></use>{' '}
-                </svg>
-                <p className='hero-section__offer-name'>Activities</p>
-              </div>
-              <div className='hero-section__offer'>
-                <svg className='hero-section__icon'>
-                  <use xlinkHref='/images/icons.svg#icon-transport'></use>{' '}
-                </svg>
-                <p className='hero-section__offer-name'>Transport</p>
-              </div>
+            <div className='hero-section__offer'>
+              <svg className='hero-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-hotel'></use>{' '}
+              </svg>
+              <p className='hero-section__offer-name'>Hotel</p>{' '}
+            </div>
+            <div className='hero-section__offer'>
+              <svg className='hero-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-visa'></use>{' '}
+              </svg>
+              <p className='hero-section__offer-name'>Visa</p>
+            </div>
+            <div className='hero-section__offer'>
+              <svg className='hero-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-activities'></use>{' '}
+              </svg>
+              <p className='hero-section__offer-name'>Activities</p>
+            </div>
+            <div className='hero-section__offer'>
+              <svg className='hero-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-transport'></use>{' '}
+              </svg>
+              <p className='hero-section__offer-name'>Transport</p>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <section className='info-section container'>
-          <div className='info-section__container'>
-            <div className='info-section__description'>
-              <h2 className='section-title'>Who We Are</h2>
-              <p>
-                Aenean feugiat ante ident augue bibendum, bibendum interdum
-                dunatont fermentum. Integer auctor enim eget excet eleifend
-                tristique. Suspendisse sed elit tortor. Nunc luctus, tellus
-                acces elementum accumsan, diam dolor accumsan eros, sit amet
-                porttitor diam ante ac augue.
-              </p>
-              <div className='info-section__advantages'>
-                <div className='info-section__advantage'>
-                  <svg className='info-section__icon'>
-                    <use xlinkHref='/images/icons.svg#icon-safety'></use>{' '}
-                  </svg>
-                  <div>
-                    <h3 className='info-section__advantage-title'>
-                      Safety First Always
-                    </h3>
-                    <p>
-                      Praesent gravida nunc at tortor cursus, molestie dapibus
-                      purus posuere. Vestibulum commodo, massa eget rutrum
-                      feugiat
-                    </p>
-                  </div>
+      <section className='info-section container'>
+        <div className='info-section__container'>
+          <div className='info-section__description'>
+            <h2 className='section-title'>Who We Are</h2>
+            <p>
+              Aenean feugiat ante ident augue bibendum, bibendum interdum
+              dunatont fermentum. Integer auctor enim eget excet eleifend
+              tristique. Suspendisse sed elit tortor. Nunc luctus, tellus acces
+              elementum accumsan, diam dolor accumsan eros, sit amet porttitor
+              diam ante ac augue.
+            </p>
+            <div className='info-section__advantages'>
+              <div className='info-section__advantage'>
+                <svg className='info-section__icon'>
+                  <use xlinkHref='/images/icons.svg#icon-safety'></use>{' '}
+                </svg>
+                <div>
+                  <h3 className='info-section__advantage-title'>
+                    Safety First Always
+                  </h3>
+                  <p>
+                    Praesent gravida nunc at tortor cursus, molestie dapibus
+                    purus posuere. Vestibulum commodo, massa eget rutrum feugiat
+                  </p>
                 </div>
-                <div className='info-section__advantage'>
-                  <svg className='info-section__icon'>
-                    <use xlinkHref='/images/icons.svg#icon-guide'></use>{' '}
-                  </svg>
-                  <div>
-                    <h3 className='info-section__advantage-title'>
-                      Trusted Travel Guide
-                    </h3>
-                    <p>
-                      Praesent gravida nunc at tortor cursus, molestie dapibus
-                      purus posuere. Vestibulum commodo, massa eget rutrum
-                      feugiat
-                    </p>
-                  </div>
+              </div>
+              <div className='info-section__advantage'>
+                <svg className='info-section__icon'>
+                  <use xlinkHref='/images/icons.svg#icon-guide'></use>{' '}
+                </svg>
+                <div>
+                  <h3 className='info-section__advantage-title'>
+                    Trusted Travel Guide
+                  </h3>
+                  <p>
+                    Praesent gravida nunc at tortor cursus, molestie dapibus
+                    purus posuere. Vestibulum commodo, massa eget rutrum feugiat
+                  </p>
                 </div>
-                <div className='info-section__advantage'>
-                  <svg className='info-section__icon'>
-                    <use xlinkHref='/images/icons.svg#icon-calendar'></use>
-                  </svg>
-                  <div>
-                    <h3 className='info-section__advantage-title'>
-                      Time And Stress Savings
-                    </h3>
-                    <p>
-                      Praesent gravida nunc at tortor cursus, molestie dapibus
-                      purus posuere. Vestibulum commodo, massa eget rutrum
-                      feugiat
-                    </p>
-                  </div>
+              </div>
+              <div className='info-section__advantage'>
+                <svg className='info-section__icon'>
+                  <use xlinkHref='/images/icons.svg#icon-calendar'></use>
+                </svg>
+                <div>
+                  <h3 className='info-section__advantage-title'>
+                    Time And Stress Savings
+                  </h3>
+                  <p>
+                    Praesent gravida nunc at tortor cursus, molestie dapibus
+                    purus posuere. Vestibulum commodo, massa eget rutrum feugiat
+                  </p>
                 </div>
               </div>
             </div>
-            <div className='info-section__photos'>
-              <img
-                src='/images/about-video-img.jpg'
-                className='info-section__video-photo'
-                alt='Photo'
-              ></img>
-              <img
-                src='/images/about-img.jpg'
-                className='info-section__photo'
-                alt='Photo'
-              ></img>
+          </div>
+          <div className='info-section__photos'>
+            <img
+              src='/images/about-video-img.jpg'
+              className='info-section__video-photo'
+              alt='Photo'
+            ></img>
+            <img
+              src='/images/about-img.jpg'
+              className='info-section__photo'
+              alt='Photo'
+            ></img>
+          </div>
+        </div>
+      </section>
+
+      <section className='hotels-section container'>
+        <div className='hotels-section__container'>
+          <h2 className='section-title'>Ultimate Travel Experience</h2>
+          <p className='hotels-section__text'>
+            Etiam ac tortor id purus commodo vulputate. Vestibulum porttitor
+            erat felis and sed vehicula tortor malesuada gravida. Mauris
+            volutpat enim quis.
+          </p>
+          <div className='hotels-section__icons'>
+            <div className='hotels-section__item'>
+              <svg className='hotels-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-tour'></use>{' '}
+              </svg>
+              <p>Tour Package</p>
+            </div>
+            <div className='hotels-section__item'>
+              <svg className='hotels-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-hotel'></use>{' '}
+              </svg>
+              <p>Hotel</p>
+            </div>
+            <div className='hotels-section__item'>
+              <svg className='hotels-section__icon'>
+                <use xlinkHref='/images/icons.svg#icon-transport'></use>{' '}
+              </svg>
+              <p>Transport</p>
             </div>
           </div>
-        </section>
-      </>
+
+          <div className='hotels-section__slider'>
+            <HotelsSlider />
+          </div>
+          <button className='book-button'>View All Hotel</button>
+        </div>
+      </section>
     </section>
   );
 };
